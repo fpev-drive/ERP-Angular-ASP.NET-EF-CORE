d9dcc3630f99d60f24189d8db1806f5a7dd3d3ed
Jordan Rigo <jordan.rigo@blueskygrs.com>
2019-02-15 15:07:11 +0100

    Update Angular project dependancies - WORK

--- ERP.SPA/src/app/app.component.spec.ts
+++ ERP.SPA/src/app/app.component.spec.ts
@@ -1,17 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import { NavComponent } from './nav/nav.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { createTranslateLoader } from './app.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+        ,NavComponent
       ],
+      imports: [ RouterTestingModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+              provide: TranslateLoader,
+              useFactory: (createTranslateLoader),
+              deps: [HttpClient]
+          }
+        }) ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeTruthy(); 
   }));
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
